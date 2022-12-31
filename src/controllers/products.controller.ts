dc4453f90943079362f1061c7f7872b046fea39a
Manuel Calle <callemac88@gmail.com>
2022-12-31 06:16:26 -0500

    all controllers

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getProduct(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: number,
+  ) {
+    return `products limit => ${limit} offset => ${offset} brand => ${brand}`;
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return 'Yo soy un filter';
+  }
+
+  @Get(':productId')
+  getProducts(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+}
