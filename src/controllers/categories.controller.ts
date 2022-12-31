b748333400b87aa8e80105c0ab8e8dc09656d992
Manuel Calle <callemac88@gmail.com>
2022-12-31 06:35:38 -0500

    Post, Put, Delete

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,9 +1,52 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
   @Get(':id/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and ${id}`;
+    return {
+      message: `product ${productId} and ${id}`,
+    };
+  }
+
+  @Get()
+  get(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: number,
+  ) {
+    return {
+      message: `categories limit => ${limit} offset => ${offset} brand => ${brand}`,
+    };
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return {
+      message: `category ${id}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'acción de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: number) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return {
+      id,
+    };
   }
 }
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,23 +1,61 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
+  @Get('filter')
+  getProductFilter() {
+    return {
+      message: 'Yo soy un filter',
+    };
+  }
+
   @Get()
   getProduct(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: number,
   ) {
-    return `products limit => ${limit} offset => ${offset} brand => ${brand}`;
-  }
-
-  @Get('filter')
-  getProductFilter() {
-    return 'Yo soy un filter';
+    return {
+      message: `products limit => ${limit} offset => ${offset} brand => ${brand}`,
+    };
   }
 
   @Get(':productId')
-  getProducts(@Param('productId') productId: string) {
-    return `product ${productId}`;
+  getOne(@Param('productId') productId: string) {
+    return {
+      message: `product ${productId}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'acción de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: number) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return {
+      id,
+    };
   }
 }
